1e51b9d2731ae5433bf7668f167607acaa36e085
Matheus Pereira <matheuspereira.azapfy@gmail.com>
2024-03-23 16:56:48 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+});
+
+
+connection.query('CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))', (err, results, fields) => {
+    if (err) throw err;
+    console.log('Table created or already exists.');
+});
+
+app.get('/add/:name', (req, res) => {
+    const name = req.params.name;
+    connection.query('INSERT INTO people (name) VALUES (?)', [name], (err, results, fields) => {
+        if (err) throw err;
+        console.log('Name added to the database:', name);
+        res.send('<h1>Full Cycle Rocks!</h1><br><br>List of names added to the database:<br>');
+    });
+});
+
+
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM people', (err, results, fields) => {
+        if (err) throw err;
+        console.log('Retrieved names from the database:', results);
+        let names = '';
+        results.forEach(result => {
+            names += result.name + '<br>';
+        });
+        res.send('<h1>Full Cycle Rocks!</h1><br><br>List of names from the database:<br>' + names);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
